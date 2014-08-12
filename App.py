5542ac041e10282c35c865c9ce7d90fd496e6aa6
Adam Kenneweg <akennewe@uwaterloo.ca>
2014-08-11 23:00:02 -0400

    Gui is now working
    next is adding a chart displaying found users

--- App.py
+++ App.py
@@ -1,63 +1,90 @@
-
+import threading
 from tkinter import *
 import SpiderCrawler
 
 
-class Application:
-    ## creating application
+class Application():
     def __init__(self):
-    	app = Tk()
-    	options = Frame(app)
-    	itemoptions = Frame(app)
-    	hoursoption = Frame(app)
-    	SchemaUpdate = BooleanVar()
-    	reset = BooleanVar()
-    	genuine = BooleanVar()
-    	buds = BooleanVar()
-    	bills = BooleanVar()
-    	unusual = BooleanVar()
-    	maxs = BooleanVar()
-    	bmoc = BooleanVar()
-    	salvage = BooleanVar()
-    	entryid = StringVar()
+        self.app = Tk()
+        startn = Frame(self.app)
+        options = Frame(self.app)
+        itemoptions = Frame(self.app)
+        hoursoption = Frame(self.app)
+        SchemaUpdate = BooleanVar()
+        reset = BooleanVar()
+        genuine = BooleanVar()
+        buds = BooleanVar()
+        bills = BooleanVar()
+        unusual = BooleanVar()
+        maxs = BooleanVar()
+        bmoc = BooleanVar()
+        salvage = BooleanVar()
+        traded = BooleanVar()
+        entryid = StringVar()
 
-    	self.clicked = False
+        self.clicked = False
 
-    	def start():
-    		SpiderCrawler.start(SchemaUpdate.get(), reset.get())
+        for i in [reset, buds, unusual, maxs, salvage]:
+            i.set(True)
 
-    	def callback(event):
-    	    if (self.clicked == False):
-    	        box.delete(0, END)
-    	        box.config(fg = "black")
-    	        self.clicked = True
+        self.lbl = Label(itemoptions, text="0/0")
+        self.lbl.pack(side = TOP)
 
-    	b = Button(app, text="START", command=start)
-    	b.pack()
+        def start():
+            t1 = threading.Thread(target = SpiderCrawler.start, args = (SchemaUpdate.get(), reset.get(), 
+                entryid.get(), genuine.get(), buds.get(), bills.get(), unusual.get(), maxs.get(), bmoc.get(), salvage.get(), hours.get(), traded.get()))
+            t1.daemon = True
+            t1.start()
+            SpiderCrawler.run = True
+            b.configure(text = "STOP", command=stop)
+            self.updateGUI()
 
-    	box = Entry(options, fg = "gray")
-    	box.bind("<Button-1>", callback)
-    	box.insert(0, "Enter steamid")
-    	box.pack(side = LEFT, anchor=N, padx =25, pady = 10)
+        def stop():
+            SpiderCrawler.run = False
+            b.configure(text = "START", command=start)
 
-    	labelText=StringVar()
-    	labelText.set("Max Hours")
-    	Label(itemoptions, textvariable=labelText).pack(side=TOP)
+        def callback(event):
+            if (self.clicked == False):
+                box.delete(0, END)
+                box.config(fg = "black")
+                self.clicked = True
 
-    	directory=StringVar(None)
-    	Entry(itemoptions,textvariable=directory,width=5).pack(side=TOP, pady = (0,20))
+        b = Button(startn, text="START", command=start)
+        
 
-    	Checkbutton(options, text = "Reload item schema", variable = SchemaUpdate).pack(side=TOP, anchor=W, fill=Y)
-    	Checkbutton(options, text = "Start with fresh id", variable = reset).pack(side=TOP, anchor=W, fill=Y)
-    	Checkbutton(itemoptions, text = "Genuines", variable = genuine).pack(side=TOP, anchor=W, fill=Y)
-    	Checkbutton(itemoptions, text = "Earbuds", variable = buds).pack(side=TOP, anchor=W, fill=Y)
-    	Checkbutton(itemoptions, text = "Bill's", variable = bills).pack(side=TOP, anchor=W, fill=Y)
-    	Checkbutton(itemoptions, text = "Unusuals", variable = unusual).pack(side=TOP, anchor=W, fill=Y)
-    	Checkbutton(itemoptions, text = "Max's items", variable = maxs).pack(side=TOP, anchor=W, fill=Y)
-    	Checkbutton(itemoptions, text = "BMOCs", variable = bmoc).pack(side=TOP, anchor=W, fill=Y)
-    	Checkbutton(itemoptions, text = "Salvaged crates", variable = salvage).pack(side=TOP, anchor=W, fill=Y)
+        box = Entry(startn, textvariable=entryid, fg = "gray")
+        box.bind("<Button-1>", callback)
+        box.insert(0, "Enter steamid")
+        box.pack(anchor=N, padx =25, pady = 10)
+        b.pack(side = BOTTOM)
 
-    	options.pack(side=LEFT, anchor=N, padx=(0,300))
-    	itemoptions.pack(side=LEFT, fill=BOTH, expand=YES, pady=(5,0))
+        startn.pack(side=LEFT, anchor = N)
 
-    	app.mainloop()
+        labelText=StringVar()
+        labelText.set("Max Hours")
+        Label(itemoptions, textvariable=labelText).pack(side=TOP)
+
+        hours=IntVar()
+        hours.set(250)
+        Entry(itemoptions,textvariable=hours,width=5).pack(side=TOP, pady = (0,20))
+
+        Checkbutton(options, text = "Reload item schema", variable = SchemaUpdate).pack(side=TOP, anchor=W, fill=Y)
+        Checkbutton(options, text = "Start with fresh id", variable = reset).pack(side=TOP, anchor=W, fill=Y)
+        Checkbutton(options, text = "Only never traded", variable = traded).pack(side=TOP, anchor=W, fill=Y)
+        Checkbutton(itemoptions, text = "Genuines", variable = genuine).pack(side=TOP, anchor=W, fill=Y)
+        Checkbutton(itemoptions, text = "Earbuds", variable = buds).pack(side=TOP, anchor=W, fill=Y)
+        Checkbutton(itemoptions, text = "Bill's", variable = bills).pack(side=TOP, anchor=W, fill=Y)
+        Checkbutton(itemoptions, text = "Unusuals", variable = unusual).pack(side=TOP, anchor=W, fill=Y)
+        Checkbutton(itemoptions, text = "Max's items", variable = maxs).pack(side=TOP, anchor=W, fill=Y)
+        Checkbutton(itemoptions, text = "BMOCs", variable = bmoc).pack(side=TOP, anchor=W, fill=Y)
+        Checkbutton(itemoptions, text = "Salvaged crates", variable = salvage).pack(side=TOP, anchor=W, fill=Y)
+
+        options.pack(side=LEFT, anchor=N, padx=(0,300))
+        itemoptions.pack(side=LEFT, fill=BOTH, expand=YES, pady=(5,0))
+
+        self.app.mainloop()
+
+    def updateGUI(self):
+        msg = str(SpiderCrawler.fcount) + "/" + str(SpiderCrawler.count)
+        self.lbl["text"] = msg
+        self.lbl.after(1000, self.updateGUI)
--- SpiderCrawler.py
+++ SpiderCrawler.py
@@ -4,15 +4,14 @@ import requests
 from xml.dom.minidom import parseString
 import pickle
 import App
+import threading
 
-fake = ['267', '266']
 STEAM_API_KEY = '32EADD85E6F53CB6AAF6D21558ED6C73' #your steam api key
-BACKPACK_TF_API_KEY = '53e1698f4f96f4977e8b4567'
-STEAM_USERNAME = 'adamater' #initial steam name
-target = 250 #maximum hours
 gameid = '440' #tf2 is 440
-wanted = '222' #Items you are looking for
 itemschema = {}
+run = True
+count = 0
+fcount = 0
 
 def schema(tf):#get item schema to find item names
 	global STEAM_API_KEY
@@ -76,20 +75,42 @@ def hours(id): #find steam hours
 			return minutes/60
 	return 0
 
-def backpack(id): # check backpack
-	global STEAM_API_KEY, gameid, found
+def backpack(id, gen, bud, bill, unu, maxs, bmoc, salv, traded): # check backpack
+	global STEAM_API_KEY, gameid, found, fcount
 	backpack_r = urllib2.urlopen(('http://api.steampowered.com/IEconItems_{}/GetPlayerItems/v0001/?key={}&steamid={}&format=xml').format(gameid, STEAM_API_KEY, id))
 	backpack = backpack_r.read()
 	data = ET.fromstring(backpack)
 	for item in data.findall("./items/item"):
-		if (item.find('quality').text.startswith('5') and int(item.find('defindex').text) not in fake):
-			found.append(id)
-			print("Unusual " + itemschema[item.find('defindex').text])
-			break
-		elif(item.find('defindex').text in wanted):
-			found.append(id)
-			print(itemschema[item.find('defindex').text])
-			break
+		if (item.find('quality').text.startswith('5') and int(item.find('defindex').text) not in [267, 266] and unu):
+			pass
+		elif(item.find('quality').text.startswith('1') and gen):
+			pass
+		elif(int(item.find('defindex').text) == 143 and bud):
+			pass
+		elif(int(item.find('defindex').text) == 126 and bill):
+			pass
+		elif(int(item.find('defindex').text) in [160,161,162] and maxs):
+			pass
+		elif(int(item.find('defindex').text) == 666 and bmoc):
+			pass
+		elif(int(item.find('defindex').text) == 5068 and salv):
+			pass
+		else:
+			continue
+		if traded and not int(item.find('id').text) == int(item.find('original_id').text):
+			continue
+		found.append(id)
+		print(itemschema[item.find('defindex').text])
+		fcount+= 1
+		break
+
+def original(item):
+	if int(item.find('id').text) == int(item.find('original_id').text):
+		return True
+	else:
+		return False
+
+
 
 def files(): #save lists to files
 	global past, future, found
@@ -101,28 +122,31 @@ def files(): #save lists to files
 	    out_file.write('\n'.join(found))
 
 if __name__ == '__main__':
+	global app
 	app = App.Application()
 
-def start(schea, res):
-	global past, future, found, itemschema
+def start(schea, res, id, gen, bud, bill, unu, maxs, bmoc, salv, hour, traded):
+	global past, future, found, itemschema, run, append, count
 	itemschema = schema(schea)
 	reset(res)
-	if STEAM_USERNAME != '':
-		tempid = getid(STEAM_USERNAME)
+	if id != '':	
+		tempid = getid(id)
 		if tempid not in past:
 			future.append(tempid)
-	count = 0
 	while len(future) != 0:
 		for i in future:
-			count +=1
-			if count%1 == 0:
-				print (str(count) + " checked")
-			files()
-			future.remove(i)
-			if i in past:
-				break
+			if run:
+				count +=1
+				if count%1 == 0:
+					print (str(count) + " checked")
+				files()
+				future.remove(i)
+				if i in past:
+					break
+				else:
+					past.append(i)
+					getfriend(i)
+					if hours(i)<hour:
+						backpack(i, gen, bud, bill, unu, maxs, bmoc, salv, traded)
 			else:
-				past.append(i)
-				getfriend(i)
-				if hours(i)<target:
-					backpack(i)
+				return
